567c345769418c3d994f8d3246e676f42ad44d2d
Jean-Baptiste Onofré <jb@nanthrax.net>
2018-10-25 06:38:28 +0200

    Move service interface in common, use generic resource service definition

---
+++ common/src/main/java/org/apache/karaf/vineyard/common/ResourceRegistryService.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.karaf.vineyard.common;
+
+import java.util.Collection;
+
+/**
+ * Generic service managing resource registry.
+ */
+public interface ResourceRegistryService<ResourceT> {
+
+  /**
+   * Add a new resource in the registry.
+   *
+   * @param resource The resource to add in the registry.
+   * @return The API created.
+   */
+  ResourceT add(ResourceT resource) throws Exception;
+
+  /**
+   * Delete an existing resource from the registry.
+   *
+   * @param resource The resource to remove.
+   */
+  void delete(ResourceT resource);
+
+  /**
+   * Delete an existing resource from the registry, identified by ID.
+   *
+   * @param id The resource ID.
+   */
+  void delete(String id);
+
+  /**
+   * Update an existing resource.
+   *
+   * @param resource The resource details.
+   */
+  void update(ResourceT resource);
+
+  /**
+   * Retrieve resource details.
+   *
+   * @param id The resource ID.
+   * @return The resource description.
+   */
+  ResourceT get(String id);
+
+  /**
+   * Retrieve all the resources in the registry.
+   *
+   * @return The list of resources.
+   */
+  Collection<ResourceT> list();
+
+}
